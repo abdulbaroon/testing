6d1c90533514ca540e2060a5019802253f5b320b
Mayank07-HP <mayank07.hp@gmail.com>
2024-05-10 19:19:59 +0530

    sadfasd

--- src/components/ui/home/intro/intro1/index.tsx
+++ src/components/ui/home/intro/intro1/index.tsx
@@ -83,7 +83,7 @@ const IntroSection1: React.FC = () => {
     <section>
       <section id="how-we-protect" ref={hiddenRef} dir={locale === "he" ? "rtl" : "ltr"}>
         <div className={styles.mainWrapper}>
-          <h2 className={classNames(styles.heading, { [styles.hide]: active !== 0 })}>{t("feature1-title")}</h2>
+          <h2 className={classNames(styles.heading, { [styles.hide]: active !== 0 })}>{t("feature1-title-3")}</h2>
           <div className={styles.wrapper}>
             <div className={styles.cards}>
               <div className={styles.cardList} id="cards">
